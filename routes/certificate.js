cb10cd822c34c1058783b022f0a6b0cf4027753f
jheave <jimenezjheave123@gmail.com>
2022-09-28 15:27:55 +0800

    refractor

--- routes/certificate.js
+++ routes/certificate.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 let Certificate = require('../models/certificate.model');
 let User = require('../models/user.model');
 const axios = require("axios");
-const {mongoose} = require('mongoose');
+const { mongoose } = require('mongoose');
 
 router.route('/').get(async (req, res) => {
-    const certificates = await Certificate.find({isApprove: true})
+    const certificates = await Certificate.find({ isApprove: true })
     res.json(certificates)
 }).post(async (req, res) => {
     const newCertificate = new Certificate(req.body);
@@ -15,18 +15,8 @@ router.route('/').get(async (req, res) => {
 });
 
 router.route('/dashboard/:id').get(async (req, res) => {
-    const certificates = await Certificate.find({user: req.params.id});
+    const certificates = await Certificate.find({ user: req.params.id });
     res.json(certificates);
 })
-router.route('/confirm-signup/:id').put(async (req, res) => {
-    const updatedCertificate = await User.findByIdAndUpdate(req.params.id, {did: "akda"}, {new: true});
-    res.json(updatedCertificate)
-
-})
-
-router.route('/login/otp').post(async (req, res) => {
-
-});
-
 
 module.exports = router;
