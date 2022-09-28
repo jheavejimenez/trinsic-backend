bdb2ac1ca8d69e2c36f25c49632e01a764090183
jheave <jimenezjheave123@gmail.com>
2022-09-28 17:00:03 +0800

    feat: email invitation

--- routes/admin.js
+++ routes/admin.js
@@ -1,52 +1,15 @@
 const router = require('express').Router();
 let Certificate = require('../models/certificate.model');
-const client = require('@sendgrid/mail');
 const { credentialsClient } = require("../utils/trinsicConfigs");
-
-
-const message = (email, name, value) => {
-    return {
-        "personalizations":
-            [
-                {
-                    "to": [
-                        {
-                            "email": `${email}`,
-                            "name": `${name}`
-                        }
-                    ]
-                }
-            ],
-        "from":
-            {
-                "email":
-                    "emman@xperto.ph",
-                "name":
-                    "XPERTO"
-            }
-        ,
-        "subject":
-            `Hi ${name}, here's your certificate`,
-        "content":
-            [
-                {
-                    "type": "text/html",
-                    "value": `<a href="http://localhost:3000/wallet/login?${value}">http://localhost:3000/wallet/login?${value}</a>`
-                }
-            ]
-    }
-}
-const sendEmail = async (encodedData, email, name) => {
-    const emailData = message(email, name, encodedData);
-    client.setApiKey(process.env.SENDGRID_API_KEY);
-    client.send(emailData).then(() => console.log('Mail sent successfully')).catch(error => {
-        console.error(error);
-    });
-}
 
 router.route('/').get(async (req, res) => {
     const certificates = await Certificate.find({ isApprove: false });
     res.json(certificates);
+})
+
+router.route('/connections').get(async (req, res) => {
+    let connections = await credentialsClient.listConnections(null);
+    res.json(connections);
 })
 
 router.route('/schema').get(async (req, res) => {
@@ -76,8 +39,6 @@ router.route('/schema').get(async (req, res) => {
 router.route('/:id').put(async (req, res) => {
     try {
         const { firstName, lastName, email, course, isApprove } = req.body;
-        await sendEmail(email, firstName)
-
         const update = { firstName, lastName, email, course };
         const updatedCertificate = await Certificate.findByIdAndUpdate(req.params.id, update, { new: true });
         res.json(updatedCertificate)
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,69 @@
 const router = require('express').Router();
 let User = require('../models/user.model');
+const client = require('@sendgrid/mail');
+const { credentialsClient } = require("../utils/trinsicConfigs");
+
+const message = (email, value) => {
+    return {
+        "personalizations":
+            [
+                {
+                    "to": [
+                        {
+                            "email": `${email}`,
+                        }
+                    ]
+                }
+            ],
+        "from":
+            {
+                "email":
+                    "emman@xperto.ph",
+                "name":
+                    "XPERTO"
+            }
+        ,
+        "subject":
+            `Hi please scan the image below to join XPERTO`,
+        "content":
+            [
+                {
+                    "type": "text/html",
+                    "value": `<img alt="QR Code" src="${value}"/>`
+                }
+            ]
+    }
+}
+
+const sendEmail = async (encodedData, email) => {
+    const emailData = message(email, encodedData);
+    client.setApiKey(process.env.SENDGRID_API_KEY);
+    client.send(emailData).then(() => console.log('Mail sent successfully')).catch(error => {
+        console.error(error);
+    });
+}
 
 router.route('/').post(async (req, res) => {
     try {
-        const user = await User.findOne({email: req.body.email});
+        const user = await User.findOne({ email: req.body.email });
         if (user) {
             res.json(user);
         } else {
             const newUser = new User(req.body);
             await newUser.save();
+
+            let connection = await credentialsClient.createConnection({
+                name: null,
+                connectionId: null,
+                multiParty: false
+            });
+
+            let qrCodeUrl = `https://chart.googleapis.com/chart?cht=qr&chl=${connection.invitationUrl}&chs=300x300&chld=L|1`
+            await sendEmail(qrCodeUrl, req.body.email);
+
             res.json(newUser);
         }
-    } catch(err) {
+    } catch (err) {
         res.status(400).json('error')
     }
 });
